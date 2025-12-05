6b203ff98e77ebf6f100c9a6c38f5cc56706385f
alpaim <156456776+alpaim@users.noreply.github.com>
2025-12-05 15:12:45 +0200

    integrate into tauri setup + state

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,3 +1,10 @@
+use tauri::Manager;
+
+use crate::state::AppState;
+
+mod state;
+mod database;
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -8,6 +15,20 @@ fn greet(name: &str) -> String {
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
+        .setup(|app| {
+            let app_handle = app.handle();
+            let app_dir = app_handle.path().app_data_dir().expect("failed to get app data dir");
+
+            tauri::async_runtime::block_on(async {
+                let pool = database::init::initialize_database(&app_dir).await
+                    .expect("failed to initialize database");
+                
+                let state = AppState::new(pool);
+                app.manage(state);
+            });
+
+            Ok(())
+        })
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
