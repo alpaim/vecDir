73f9f0858ba797129ccd3e4dbdf364695e90a1dc
alpaim <156456776+alpaim@users.noreply.github.com>
2025-12-05 13:26:38 +0200

    vite alias path fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
+import path from "node:path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-// @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vite.dev/config/
@@ -27,6 +27,11 @@ export default defineConfig(async () => ({
         watch: {
             // 3. tell Vite to ignore watching `src-tauri`
             ignored: ["**/src-tauri/**"],
+        },
+    },
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
         },
     },
 }));
