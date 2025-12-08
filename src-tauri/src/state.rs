78cba28a3278819b090e61913533cc3c7ad27282
alpaim <156456776+alpaim@users.noreply.github.com>
2025-12-08 21:26:50 +0200

    rust-ai: add AI to AppState

--- src-tauri/src/state.rs
+++ src-tauri/src/state.rs
@@ -1,17 +1,19 @@
 use std::path::PathBuf;
 
-use crate::{database::DbPool, vector_store::manager::VectorIndexManager};
+use crate::{ai::AI, database::DbPool, vector_store::manager::VectorIndexManager};
 
 pub struct AppState {
     pub db: DbPool,
     pub vector_index_manager: VectorIndexManager,
+    pub openai_client: AI,
 }
 
 impl AppState {
-    pub fn new(db: DbPool, app_dir: PathBuf) -> Self {
+    pub fn new(db: DbPool, app_dir: PathBuf, openai_client: AI) -> Self {
         Self { 
             db,
             vector_index_manager: VectorIndexManager::new(app_dir),  
+            openai_client: openai_client
         }
     }
 }
