d58e89534871e95c4807c68c854ebe33cfc0572f
alpaim <156456776+alpaim@users.noreply.github.com>
2025-12-08 17:32:49 +0200

    vector: DashMap instead of Mutex<HashMap>

--- src-tauri/src/vector_store/manager.rs
+++ src-tauri/src/vector_store/manager.rs
@@ -1,16 +1,16 @@
 use std::{
-    collections::HashMap,
     path::PathBuf,
     sync::{Arc, Mutex},
 };
 
 use anyhow::{Ok, Result};
+use dashmap::{DashMap, Entry};
 use usearch::IndexOptions;
 
 use crate::vector_store::VectorStore;
 
 pub type VectorStoreArcMutex = Arc<Mutex<VectorStore>>;
-pub type IndiciesMapMutex = Mutex<HashMap<i64, VectorStoreArcMutex>>;
+pub type IndiciesMapMutex = DashMap<i64, VectorStoreArcMutex>;
 
 pub struct VectorIndexManager {
     indices: IndiciesMapMutex,
@@ -20,34 +20,33 @@ pub struct VectorIndexManager {
 impl VectorIndexManager {
     pub fn new(app_dir: PathBuf) -> Self {
         Self {
-            indices: Mutex::new(HashMap::new()),
+            indices: DashMap::new(),
             app_dir,
         }
     }
 
     pub fn get_index(&self, space_id: i64) -> Result<VectorStoreArcMutex> {
-        let mut map = self.indices.lock().unwrap();
+        match self.indices.entry(space_id) {
+            Entry::Occupied(entry) => Ok(entry.get().clone()),
+            Entry::Vacant(entry) => {
+                // Need to make a function to generate index options from space db info
+                let options = IndexOptions {
+                    dimensions: 1536,
+                    metric: usearch::MetricKind::Cos,
+                    quantization: usearch::ScalarKind::I8,
+                    connectivity: 16,
+                    expansion_add: 128,
+                    expansion_search: 64,
+                    multi: false,
+                };
 
-        if let Some(store) = map.get(&space_id) {
-            return Ok(store.clone());
+                let store = VectorStore::init(&self.app_dir, space_id, options)?;
+                let store_arc: VectorStoreArcMutex = Arc::new(Mutex::new(store));
+
+                entry.insert(store_arc.clone());
+
+                Ok(store_arc)
+            }
         }
-
-        // Need to make a function to generate index options from space db info
-        let options = IndexOptions {
-            dimensions: 1536,
-            metric: usearch::MetricKind::Cos,
-            quantization: usearch::ScalarKind::I8,
-            connectivity: 16,
-            expansion_add: 128,
-            expansion_search: 64,
-            multi: false,
-        };
-
-        let store = VectorStore::init(&self.app_dir, space_id, options)?;
-        let store_arc: VectorStoreArcMutex = Arc::new(Mutex::new(store));
-
-        map.insert(space_id, store_arc.clone());
-
-        Ok(store_arc)
     }
 }
