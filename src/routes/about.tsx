46af26c4dcea4edf6428011e4745125e2c94448d
alpaim <156456776+alpaim@users.noreply.github.com>
2025-12-09 16:01:55 +0200

    fe: tanstack router initial implementation

---
+++ src/routes/about.tsx
@@ -0,0 +1,12 @@
+import { createRoute } from "@tanstack/react-router";
+import { rootRoute } from "@/routes/_root";
+
+function About() {
+    return <div className="p-2">Hello from About!</div>;
+}
+
+export const aboutRoute = createRoute({
+    getParentRoute: () => rootRoute,
+    path: "/about",
+    component: About,
+});
