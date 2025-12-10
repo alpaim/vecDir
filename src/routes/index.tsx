ad74ddd733d314785e8cb3b9f73ec7d79a5bb1ad
alpaim <156456776+alpaim@users.noreply.github.com>
2025-12-10 21:01:32 +0200

    fe: basic searchQuery state implementation

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,17 +1,38 @@
 import { createRoute } from "@tanstack/react-router";
+import { useState } from "react";
 import { Input } from "@/components/ui/input";
 import { rootRoute } from "@/routes/root";
 
+function SearchResult({ searchQuery}: { searchQuery: string }) {
+    return (
+        <div>
+            Your query:
+            {" "}
+            {searchQuery}
+        </div>
+    );
+}
+
+function Logo() {
+    return (
+        <div>
+            <h3 className="font-mono text-2xl font-extrabold">VecDir</h3>
+        </div>
+    );
+}
+
 function Index() {
+    const [searchQeury, setSearchQuery] = useState<string | undefined>();
+
     return (
         <div className="relative flex items-center justify-center h-full w-full">
             <div className="absolute top-5 left-0 w-full px-5">
-                <Input placeholder="Search..." className="font-mono" />
+                <Input placeholder="Search..." className="font-mono" onChange={e => setSearchQuery(e.target.value)} value={searchQeury} />
             </div>
             <div className="flex">
-                <div>
-                    <h3 className="font-mono text-2xl font-extrabold">VecDir</h3>
-                </div>
+                {
+                    (searchQeury !== undefined && searchQeury !== "") ? <SearchResult searchQuery={searchQeury} /> : <Logo />
+                }
             </div>
         </div>
     );
