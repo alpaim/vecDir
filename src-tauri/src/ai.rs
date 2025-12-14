882c0a7d590a12a1ab167b464778061ede05e6e9
alpaim <156456776+alpaim@users.noreply.github.com>
2025-12-14 22:12:31 +0200

    rust-ai: add describe_image_from_file,  image_to_base64 functions

--- src-tauri/src/ai.rs
+++ src-tauri/src/ai.rs
@@ -1,12 +1,20 @@
+use std::path::Path;
+
 use anyhow::{Context, Ok, Result};
 use async_openai::{
     config::OpenAIConfig,
     types::{
+        chat::{
+            ChatCompletionRequestMessageContentPartImage,
+            ChatCompletionRequestMessageContentPartText, ChatCompletionRequestUserMessageArgs,
+            CreateChatCompletionRequestArgs, ImageDetail, ImageUrl,
+        },
         embeddings::{CreateEmbeddingRequestArgs, CreateEmbeddingResponse},
-        responses::{CreateResponseArgs, Response},
+        responses::{CreateResponseArgs, InputParam, Response},
     },
     Client,
 };
+use base64::Engine;
 
 pub mod embedding;
 pub mod llm;
@@ -68,7 +76,7 @@ impl AI {
         Ok(response)
     }
 
-    pub async fn request_llm(&self, input: String, model: String) -> Result<Response> {
+    pub async fn request_llm(&self, input: InputParam, model: String) -> Result<Response> {
         let args = CreateResponseArgs::default()
             .input(input)
             .model(model)
@@ -83,5 +91,76 @@ impl AI {
             .context("failed to receive result of llm request")?;
 
         Ok(response)
+    }
+
+    pub async fn image_to_base64(&self, file_path: &str) -> Result<String> {
+        let image_path = Path::new(file_path);
+
+        let image_data = tokio::fs::read(image_path)
+            .await
+            .context("failed to read image to parse it to base64")?;
+
+        let mime_type = match image_path.extension().and_then(|ext| ext.to_str()) {
+            Some("png") => "image/png",
+            Some("jpg") | Some("jpeg") => "image/jpeg",
+            Some("webp") => "image/webp",
+            Some("gif") => "image/gif",
+            _ => "image/jpeg",
+        };
+
+        let b64 = base64::engine::general_purpose::STANDARD.encode(&image_data);
+
+        let data_url = format!("data:{};base64,{}", mime_type, b64);
+
+        Ok(data_url)
+    }
+
+    pub async fn describe_image_from_file(
+        &self,
+        file_path: &str,
+        prompt: &str,
+        model: String,
+    ) -> Result<String> {
+        let image_url = self
+            .image_to_base64(file_path)
+            .await
+            .context("failed to get image base64 url")?;
+
+        let request = CreateChatCompletionRequestArgs::default()
+            .model(model)
+            .messages([ChatCompletionRequestUserMessageArgs::default()
+                .content(vec![
+                    ChatCompletionRequestMessageContentPartText::from(prompt).into(),
+                    ChatCompletionRequestMessageContentPartImage::from(ImageUrl {
+                        url: image_url,
+                        detail: Some(ImageDetail::Auto),
+                    })
+                    .into(),
+                ])
+                .build()
+                .context("failed to build content content for llm image description request")?
+                .into()])
+            .build()
+            .context("failed to build llm request for image description")?;
+
+        let response = self
+            .client
+            .chat()
+            .create(request)
+            .await
+            .context("failed to receive result of llm image description")?;
+
+        let llm_response_message = response
+            .choices
+            .last()
+            .context("failed to get latest llm message")?
+            .clone();
+
+        let llm_response_text = llm_response_message
+            .message
+            .content
+            .context("failed to get content of llm message")?;
+
+        Ok(llm_response_text)
     }
 }
