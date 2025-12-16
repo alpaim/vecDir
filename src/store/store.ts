82597e6db6575a6789602ddb198f96408fbdc6fb
alpaim <156456776+alpaim@users.noreply.github.com>
2025-12-16 14:53:38 +0200

    fe: add spaces to state

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,21 +1,30 @@
-import type { Space } from "@/lib/vecdir/bindings";
+import type { EmbeddingConfig, LLMConfig, Space } from "@/lib/vecdir/bindings";
 import type { AppConfig } from "@/types/config";
 import { create } from "zustand";
 
 interface AppState {
     isBackendReady: boolean;
+
     config: AppConfig;
-    spaces: Space[];
+
+    spaces: Map<number, Space>;
+    selectedSpace: number;
 
     setBackendStatus: (isReady: boolean) => void;
+
     setConfig: (config: AppConfig) => Promise<void>;
+
+    setSpaces: (spaces: Space[]) => void;
+    addSpace: (space: Space) => void;
+    selectSpace: (spaceId: number) => void;
 }
 
 // TODO: make persist with automatic syncing with getConfig from backend: https://zustand.docs.pmnd.rs/integrations/persisting-store-data#typescript-simple-example
 export const useAppState = create<AppState>()(set => ({
     isBackendReady: false,
 
-    spaces: [],
+    spaces: new Map(),
+    selectedSpace: 0,
 
     config: { version: "" },
 
@@ -28,5 +37,28 @@ export const useAppState = create<AppState>()(set => ({
         await new Promise(resolve => setTimeout(resolve, 500));
 
         set({ config });
+    },
+
+    setSpaces: (spaces) => {
+        const spacesMap = new Map<number, Space>();
+
+        for (const space of spaces) {
+            spacesMap.set(space.id, space);
+        }
+
+        set({ spaces: spacesMap });
+    },
+
+    addSpace: (space) => {
+        set(state => ({
+            spaces: {
+                ...state.spaces,
+                [space.id]: space,
+            },
+        }));
+    },
+
+    selectSpace: (spaceId) => {
+        set({ selectedSpace: spaceId });
     },
 }));
