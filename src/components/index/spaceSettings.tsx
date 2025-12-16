1626b03c3d5833cbd15f00f9715ba0fe159ba346
alpaim <156456776+alpaim@users.noreply.github.com>
2025-12-16 17:18:19 +0200

    fe: add root to space logic

--- src/components/index/spaceSettings.tsx
+++ src/components/index/spaceSettings.tsx
@@ -1,11 +1,28 @@
+import type { IndexedRoot } from "@/lib/vecdir/bindings";
+import { open } from "@tauri-apps/plugin-dialog";
+import { useEffect, useState } from "react";
 import { RootsList } from "@/components/index/rootsList";
 import { CreateSpace } from "@/components/spaces/createSpace";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { addRoot } from "@/lib/vecdir/roots/createRoot";
+import { getRootsBySpaceId } from "@/lib/vecdir/roots/getRoot";
 import { useAppState } from "@/store/store";
 
 export function SpaceSettings() {
+    const [roots, setRoots] = useState<IndexedRoot[]>([]);
+
     const store = useAppState();
+
+    async function updateRoots(selectedSpaceId: number, set: (r: IndexedRoot[]) => void): Promise<void> {
+        const newRoots = await getRootsBySpaceId(selectedSpaceId);
+
+        set(newRoots);
+    }
+
+    useEffect(() => {
+        updateRoots(store.selectedSpace, setRoots).then(() => {});
+    }, []);
 
     if (store.spaces.size === 0) {
         return <CreateSpace />;
@@ -20,13 +37,28 @@ export function SpaceSettings() {
                     </CardTitle>
                 </CardHeader>
                 <CardContent>
-                    <RootsList roots={[]} />
+                    <RootsList roots={roots} />
                 </CardContent>
                 <CardFooter className="flex-col gap-2">
                     <Button
                         variant="outline"
                         className="w-full"
-                        onClick={() => {}}
+                        onClick={async () => {
+                            const path = await open({
+                                multiple: false,
+                                directory: true,
+                            });
+
+                            if (!path) {
+                                return;
+                            }
+
+                            await addRoot(store.selectedSpace, path);
+
+                            await updateRoots(store.selectedSpace, setRoots);
+
+                            console.log(path, store.spaces.get(1), store.selectedSpace);
+                        }}
                     >
                         Add Directory
                     </Button>
