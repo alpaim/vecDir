b8735303066b62e15c84c581f79f2a236b62f0ba
alpaim <156456776+alpaim@users.noreply.github.com>
2025-12-19 18:22:46 +0200

    fe: show selected space name on homepage

--- src/components/index/main.tsx
+++ src/components/index/main.tsx
@@ -1,10 +1,29 @@
+import type { Space } from "@/lib/vecdir/bindings";
 import { Link } from "@tanstack/react-router";
 import { RefreshCcw, Settings } from "lucide-react";
+import { useEffect, useState } from "react";
 import { Logo } from "@/components/index/logo";
 import { Search } from "@/components/search/search";
 import { Button } from "@/components/ui/button";
+import { processSpace } from "@/lib/vecdir/indexer/processSpace";
+import { indexSpace } from "@/lib/vecdir/indexer/startIndexing";
+import { getSpaceById } from "@/lib/vecdir/spaces/getSpace";
+import { useAppState } from "@/store/store";
+
+async function onIndexSpace(spaceId: number) {
+    await indexSpace(spaceId);
+    await processSpace(spaceId);
+}
 
 export function Main({ searchQuery }: { searchQuery: string | undefined }) {
+    const [space, setSpace] = useState<Space | undefined>();
+
+    const selectedSpace = useAppState(state => state.selectedSpace);
+
+    useEffect(() => {
+        getSpaceById(selectedSpace).then(s => setSpace(s || undefined));
+    }, [selectedSpace]);
+
     if (searchQuery !== undefined && searchQuery !== "") {
         return (
             <Search searchQuery={searchQuery} />
@@ -15,14 +34,18 @@ export function Main({ searchQuery }: { searchQuery: string | undefined }) {
         <div className="flex flex-col items-center justify-center flex-wrap w-full h-full">
             <div className="shrink-0">
                 <Logo />
+
+                <div className="text-center">
+                    Selected Space:
+                    {" "}
+                    {space?.name}
+                </div>
             </div>
             <div className="w-full justify-center shrink-0">
                 <div className="flex flex-row gap-3 justify-center pt-6">
-                    <Link to="/settings">
-                        <Button>
-                            <RefreshCcw />
-                        </Button>
-                    </Link>
+                    <Button onClick={async () => onIndexSpace(selectedSpace).then(() => {})}>
+                        <RefreshCcw />
+                    </Button>
                     <Link to="/settings">
                         <Button>
                             <Settings />
